a13f616c7e0283bb2cc355701b9aef75fa878d6a
Michael Foley <michaeldfoley@gmail.com>
2015-05-13 10:58:48 -0400

    added sponsors website field

--- shortcode.php
+++ shortcode.php
@@ -38,11 +38,15 @@ function _sbcg_sponsors( $atts, $content = '' ) {
   endif;  
   
   foreach ($posts as $post) {
+    $url = get_post_meta( $post->ID, '_sbcg_sponsors_value_key', true );
     $out .= sprintf( 
-        ( empty( $args['id'] ) ) ? "\n<li class=\"sponsor\">%s</li>\n" : "\n<div class=\"sponsor-single\">%s</div>\n", 
-        ( has_post_thumbnail( $post->ID ) ) ? "<img src=\"" . wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' )[0] . "\" alt=\"" . get_the_title( $post->ID ) . "\">" : get_the_title( $post->ID )
+        ( empty( $args['id'] ) ) ? "\n<li class=\"sponsor\">%s%s%s</li>" : "\n<div class=\"sponsor-single\">%s%s%s</div>", 
+        ( $url ) ? "<a href=\"{$url}\">" : "",
+        ( has_post_thumbnail( $post->ID ) ) ? "<img src=\"" . wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' )[0] . "\" alt=\"" . get_the_title( $post->ID ) . "\">" : get_the_title( $post->ID ),
+        ( $url ) ? "</a>" : ""
       );
-  }   
+  }
+  $out .="\n";
   if ( empty( $args['id'] ) ) { return "<ul class=\"sponsors\">{$out}</ul>"; };
   
   return $out;
--- sponsors.php
+++ sponsors.php
@@ -23,40 +23,104 @@ function _sbcg_create_sponsors() {
         'singular_name' => __( 'Sponsor', '_sbcgsponsors' ),
         'add_new' => __( 'Add New', '_sbcgsponsors' ),
         'add_new_item' => __( 'Add New Sponsor', '_sbcgsponsors' ),
-        'edit' => __( 'Edit', '_sbcgsponsors' ),
         'edit_item' => __( 'Edit Sponsor', '_sbcgsponsors' ),
         'new_item' => __( 'New Sponsor', '_sbcgsponsors' ),
-        'view' => __( 'View', '_sbcgsponsors' ),
+        'all_items' => __( 'All Sponsors', '_sbcgsponsors' ),
         'view_item' => __( 'View Sponsor', '_sbcgsponsors' ),
         'search_items' => __( 'Search Sponsors', '_sbcgsponsors' ),
         'not_found' => __( 'No sponsors found', '_sbcgsponsors' ),
         'not_found_in_trash' => __( 'No sponsors found in the trash', '_sbcgsponsors' ),
-        'parent' => __( 'Parent Sponsor', '_sbcgsponsors' ),
         'parent_item_colon' => __( 'Parent Sponsor', '_sbcgsponsors' ),
-        'all_items' => __( 'All Sponsors', '_sbcgsponsors' )
+        'menu_name' => __( 'Sponsors', '_sbcgsponsors' )
       ),
       'rewrite' => array(
         'slug' => __( 'sponsors', '_sbcgsponsors' )
       ),
       'public' => true,
       'menu_position' => 30,
-      'supports' => array( 'title', 'editor', 'thumbnail' ),
-      'taxonomies' => array( 
-        __( 'Founding Sponsor', '_sbcgsponsors' ),
-      ),
+      'supports' => array( 'title', 'thumbnail' ),
+      'taxonomies' => array( ),
       'menu_icon' => 'dashicons-money',
       'has_archive' => true
     )
   );
 }
 
+
+/**
+ * Add website field
+ */
+ 
+add_action("add_meta_boxes__sbcg_sponsors", "_sbcg_add_sponsors_website");
+ 
+function _sbcg_add_sponsors_website(){
+  add_meta_box('_sbcg_sponsors_website', __( 'Sponsor Website', '_sbcgsponsors' ), '_sbcg_sponsors_website_callback', '_sbcg_sponsors', 'normal');
+}
+ 
+function _sbcg_sponsors_website_callback( $post ){
+	wp_nonce_field( '_sbcg_sponsors_website', '_sbcg_sponsors_website_nonce' );
+	
+  $value = get_post_meta( $post->ID, '_sbcg_sponsors_value_key', true );
+
+	echo '<label for="_sbcg_sponsors_website">';
+	_e( 'Website', '_sbcgsponsors' );
+	echo '</label> ';
+	echo '<input type="text" id="_sbcg_sponsors_website" name="_sbcg_sponsors_website" value="' . esc_attr( $value ) . '" style="width: calc(100% - 60px);" />';
+}
+
+function _sbcg_sponsors_meta_save( $post_id ) {
+  
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['_sbcg_sponsors_website_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['_sbcg_sponsors_website_nonce'], '_sbcg_sponsors_website' ) ) {
+		return;
+	}
+
+	// Don't save on autosave.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+	
+	// Make sure that it is set.
+	if ( ! isset( $_POST['_sbcg_sponsors_website'] ) ) {
+		return;
+	}
+
+	// Sanitize user input.
+	$my_data = esc_url_raw( $_POST['_sbcg_sponsors_website'] );
+
+	// Update the meta field in the database.
+	update_post_meta( $post_id, '_sbcg_sponsors_value_key', $my_data );
+}
+add_action( 'save_post', '_sbcg_sponsors_meta_save' );
+
+
 /**
  * Adds sponsors shortcode
  */
 require plugin_dir_path( __FILE__ ) . 'shortcode.php';
 
+
 /**
- * Adds sponsors shortcode
+ * Adds archive status
  */
 require plugin_dir_path( __FILE__ ) . 'status.php';
 ?>
